15dec9de42699fe36deebb4ec5961c6cd42c8c7b
christiangubana <christiangubana@gmail.com>
2023-11-20 21:47:46 +0200

    update look and fill

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,4 +1,3 @@
-// HeroBanner.js
 import React from "react";
 import "./HeroBanner.css";
 import { LogoSliders } from "./LogoSliders";
@@ -7,26 +6,28 @@ const HeroBanner = () => {
   return (
     <div className="hero-banner">
       <div className="container">
-        <div className="col-md-6">
-          <div className="hero-content text-center text-md-left">
-            <h6 className="text-start">Monolith</h6>
-            <h1 className="text-start">Test less. Learn more.</h1>
-            <p className="text-start">
-              Spend less time running expensive, repetitive tests and more time
-              learning from your engineering data to predict the exact tests to
-              run.
-            </p>
-          </div>
-          <div className="col-md-12">
-            <div className="d-flex justify-content-between">
-              <button className="first-button">Request a demo</button>
-              <button className="second-button">Speak to our team</button>
+        <div className="row">
+          <div className="col-md-6">
+            <div className="hero-content text-center text-md-left">
+              <h6 className="text-start">Monolith</h6>
+              <h1 className="text-start">Test less. Learn more.</h1>
+              <p className="text-start">
+                Spend less time running expensive, repetitive tests and more
+                time learning from your engineering data to predict the exact
+                tests to run.
+              </p>
+              <div className="d-flex justify-content-start">
+                <button className="first-button me-3">Request a demo</button>
+                <button className="second-button">Speak to our team</button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="col-md-12">
-        <LogoSliders />
+        <div className="row">
+          <div className="col-md-12">
+            <LogoSliders />
+          </div>
+        </div>
       </div>
     </div>
   );
