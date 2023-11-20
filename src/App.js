ac48fb2d76331dd26b284c5e3b8b92ecca7aac2b
christiangubana <christiangubana@gmail.com>
2023-11-20 19:17:26 +0200

    updated global style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import "./App.css";
 import HeroBanner from "./components/HeroBanner";
 
 function App() {
   return (
     <div className="App">
-      <h1>Monolith</h1>
       <HeroBanner />
     </div>
   );
--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,3 +1,4 @@
+// HeroBanner.js
 import React from "react";
 import "./HeroBanner.css";
 import { LogoSliders } from "./LogoSliders";
@@ -5,47 +6,34 @@ import { LogoSliders } from "./LogoSliders";
 const HeroBanner = () => {
   return (
     <div className="hero-banner">
-      <div className="hero-content">
-        <div
-          class="span12 widget-span widget-type-custom_widget dnd-module"
-          data-widget-type="custom_widget"
-          data-x="0"
-          data-w="12"
-        >
-          <div
-            id="hs_cos_wrapper_module_165634242646012"
-            class="hs_cos_wrapper hs_cos_wrapper_widget hs_cos_wrapper_type_module widget-type-rich_text"
-            data-hs-cos-general-type="widget"
-            data-hs-cos-type="module"
-          >
-            <span
-              id="hs_cos_wrapper_module_165634242646012_"
-              class="hs_cos_wrapper hs_cos_wrapper_widget hs_cos_wrapper_type_rich_text"
-              data-hs-cos-general-type="widget"
-              data-hs-cos-type="rich_text"
-            >
-              <h6>Monolith</h6>
-              <h1
-                style={{ fontSize: "48px", fontWeight: "bold" }}
-                data-pm-slice="1 1 []"
-              >
-                <span style={{ color: " #ffffff" }}>
-                  Use AI software to test less and learn more.
-                </span>
-              </h1>
-              <p style={{ fontSize: "18px" }}>
-                <span style={{ color: " #ffffff" }}>
-                  Spend less time running expensive, repetitive tests and more
-                  time learning from your engineering data to predict the exact
-                  tests to run.
-                </span>
-              </p>
-            </span>
+      <div className="container">
+        <div className="col-md-6">
+          <div className="hero-content text-center text-md-left">
+            <h6 className="text-start">Monolith</h6>
+            <h1 className="text-start">
+              Use AI software to test less and learn more.
+            </h1>
+            <p className="text-start">
+              Spend less time running expensive, repetitive tests and more time
+              learning from your engineering data to predict the exact tests to
+              run.
+            </p>
+          </div>
+          <div className="col-md-10">
+            <div className="d-flex justify-content-between">
+              <button className="btn btn-danger text-uppercase text-white">
+                Request a demo
+              </button>
+              <button className="btn btn-primary text-uppercase text-white">
+                Speak to our team
+              </button>
+            </div>
           </div>
         </div>
-        <button>Get Started</button>
       </div>
-      <LogoSliders />
+      <div className="col-md-12">
+        <LogoSliders />
+      </div>
     </div>
   );
 };
