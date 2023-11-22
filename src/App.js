01de9b31a2be7876c6e3bd96483dcde67f9a850d
christiangubana <christiangubana@gmail.com>
2023-11-22 14:35:14 +0200

    update header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HeroBanner from "./components/HeroBanner";
 function App() {
   return (
     <div className="App">
+      <h1 className="header">Huble - Monolith Banner</h1>
       <HeroBanner />
     </div>
   );
